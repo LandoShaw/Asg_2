63edfd01d832c9d985816263ef50e97115a6c6e1
LandoShaw <landonodishaw@gmail.com>
2024-04-07 18:02:12 -0600

    component folder functioning

--- asg2-react-app/src/App.jsx
+++ asg2-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import "./style.scss"
 import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js';
+import Navbar from './components/Navbar.jsx';
 
 const supaUrl = 'https://cgsmvcmhbmmgvprycefa.supabase.co';
 const supaAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnc212Y21oYm1tZ3ZwcnljZWZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDM4MjYsImV4cCI6MjAyNjAxOTgyNn0.HGbpJ4CB3tNj_KvhRViM3a-9RhH_pPhS2aimD7_7fO8';
@@ -21,11 +22,9 @@ function App() {
   // }
 
   return (
-      <div>
-        <button className="is-success is-outlined is-rounded is-large">
-    Our Button
-</button>
-      </div>
+
+    <Navbar></Navbar>
+
   )
 }
 
