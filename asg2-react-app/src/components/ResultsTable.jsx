f6eb165e0eed644ccf3aa7f0972b157be5dd3678
UnlawfulChaos <chief.blastro@gmail.com>
2024-04-09 00:44:03 -0600

    added pictures

--- asg2-react-app/src/components/ResultsTable.jsx
+++ asg2-react-app/src/components/ResultsTable.jsx
@@ -9,9 +9,24 @@ const ResultsTable = (props) => {
     return (
         <div>
             <div>
-                <img/>
-                <img/>
-                <img/>
+                <div className="columns">
+                    <div className="column">
+                        <img src="https://placehold.co/100x100"/>
+                        <p><strong>First</strong></p>
+                        <p>{raceResultInformation[0].drivers.forename} {raceResultInformation[0].drivers.surname}</p>
+                    </div>
+                    <div className="column">
+                        <img src="https://placehold.co/100x100"/>
+                        <p><strong>First</strong></p>
+                        <p>{raceResultInformation[1].drivers.forename} {raceResultInformation[1].drivers.surname}</p>
+                    </div>
+                    <div className="column">
+                        <img src="https://placehold.co/100x100"/>
+                        <p><strong>First</strong></p>
+                        <p>{raceResultInformation[2].drivers.forename} {raceResultInformation[2].drivers.surname}</p>
+                    </div>  
+                </div>
+
             </div>
             <table className="table">
                 <thead>
